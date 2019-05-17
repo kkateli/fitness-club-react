c2cd2f528bd9a4604bddbdd9d789824f74abb386
kate li <kateli24@outlook.com>
2019-05-17 23:28:40 +1200

    Finished top header

--- fitness-shop/src/components/Nav/Nav.js
+++ fitness-shop/src/components/Nav/Nav.js
@@ -1,76 +1,110 @@
 import React, { Component } from "react";
 import Icofont from "react-icofont";
 import cssClasses from "./Nav.module.css";
+import { Link } from "react-router-dom";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 class NavBar extends Component {
   render() {
     return (
       <div>
-          
         {/* Top Header */}
         <div className={cssClasses.topHeader}>
-          <div className="container">
-            <div className="row">
-              <div className="col-md-7 col-lg-7">
-                <div className={cssClasses.addressBar}>
-                  <ul className={cssClasses.listInline}>
-                    <li>
-                      <a href={this.props.mailLink}>
-                        <Icofont icon="icofont-email" /> {this.props.mail}
-                      </a>
-                    </li>
-                    <li>
-                      <a href={this.props.numberLink}>
-                        <Icofont icon="icofont-ui-call" /> {this.props.Number}
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-
-              <div className="col-lg-5 col-md-5">
-                <div className={cssClasses.socialIcons}>
-                  <ul className={cssClasses.listInline}>
-                    <li>
-                      <a
-                        href={this.props.facebookLink}
-                        rel="noopener noreferrer"
-                        target="_blank"
-                      >
-                        <Icofont icon="icofont-facebook" />
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href={this.props.twitterLink}
-                        rel="noopener noreferrer"
-                        target="_blank"
-                      >
-                        <Icofont icon="icofont-twitter" />
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href={this.props.instagramLink}
-                        rel="noopener noreferrer"
-                        target="_blank"
-                      >
-                        <Icofont icon="icofont-instagram" />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
+          <div className={cssClasses.socialIcons}>
+            <ul className={cssClasses.listInline}>
+              <li>
+                <a href={this.props.facebookLink}>
+                  <Icofont icon="icofont-facebook" />
+                </a>
+              </li>
+              <li>
+                <a href={this.props.twitterLink}>
+                  <Icofont icon="icofont-twitter" />
+                </a>
+              </li>
+              <li>
+                <a href={this.props.instagramLink}>
+                  <Icofont icon="icofont-instagram" />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className={cssClasses.addressBar}>
+            <ul className={cssClasses.listInline}>
+              <li>
+                <a href={this.props.mailLink}>
+                  <Icofont icon="icofont-email" /> {this.props.mail}
+                </a>
+              </li>
+              <li>
+                <a href={this.props.numberLink}>
+                  <Icofont icon="icofont-ui-call" /> {this.props.Number}
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
-        </div>
-        
+      </div>
+      //   Top header ends here
     );
+    // <Navbar
+    //   id="navbar"
+    //   bg="light"
+    //   expand="lg"
+    //   className="navbar navbar-expand-md navbar-light"
+    //   collapseOnSelect={true}
+    // >
+    //   <Container>
+    //   <Navbar.Toggle
+    //       className="navbar-toggler"
+    //       type="button"
+    //       data-toggle="collapse"
+    //       data-target="#navbarSupportedContent"
+    //       aria-controls="navbarSupportedContent"
+    //       aria-expanded="false"
+    //       aria-label="Toggle navigation"
+    //       id="collaspe-btn"
+    //     />
+    //     <Navbar.Collapse id="navbarSupportedContent">
+    //       <Nav className="navbar-nav ml-auto">
+    //         <div>
+    //           <Nav.Item>
+    //             <Link
+    //               activeclass="active"
+    //               to="home"
+    //               spy={true}
+    //               smooth={true}
+    //               offset={-200}
+    //               duration={800}
+    //               className="smooths nav-link"
+    //               onClick={this.closeNavbar}
+    //             >
+    //               Home
+    //             </Link>
+    //           </Nav.Item>
+
+    //           <Nav.Item>
+    //             <Link
+    //               activeclass="active"
+    //               to="contact"
+    //               spy={true}
+    //               smooth={true}
+    //               offset={-200}
+    //               duration={800}
+    //               className="nav-link"
+    //               onClick={this.closeNavbar}
+    //             >
+    //               Contact
+    //             </Link>
+    //           </Nav.Item>
+
+    //         </div>
+    //       </Nav>
+    //     </Navbar.Collapse>
+    //   </Container>
+    //   </Navbar>
   }
 }
-
-
 
 //Default Props
 NavBar.defaultProps = {
