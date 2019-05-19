3cc0fc7245d7dee520c7a2e20d829bdd9f976d27
kate li <kateli24@outlook.com>
2019-05-19 16:05:05 +1200

    Added img uploading and preview functions

--- fitness-shop/src/components/NewPost/NewPost.js
+++ fitness-shop/src/components/NewPost/NewPost.js
@@ -1,93 +1,125 @@
 import React, { Component } from "react";
+
 class NewPost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { file: "", imagePreviewUrl: "" };
+  }
+
+  _handleSubmit(e) {
+    e.preventDefault();
+    // TODO: do something with -> this.state.file
+    console.log("handle uploading-", this.state.file);
+  }
+
+  _handleImageChange(e) {
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result
+      });
+    };
+
+    reader.readAsDataURL(file);
+  }
+
   render() {
+    let { imagePreviewUrl } = this.state;
+    let $imagePreview = null;
+    if (imagePreviewUrl) {
+      $imagePreview = <img src={imagePreviewUrl} alt="post img"/>;
+    } else {
+      $imagePreview = (
+        <div className="previewText">Please select an Image for Preview</div>
+      );
+    }
     return (
       <div className="new-post">
-        {/* <!-- Default form register --> */}
-        <form class="text-center border border-light">
+        {/* <!-- page title --> */}
+        <form className="text-center border border-light">
           <div className="section-title">
             <h2>New Post</h2>
             <p>{this.props.sectionDescription}</p>
             <span className="section-title-bg">New Post</span>
           </div>
-
-          <div class="form-row mb-4">
-            <div class="col">
-              {/* <!-- First name --> */}
+          {/* //form starts here */}
+          {/* <div className="form-row mb-4">
+            <div className="col"> */}
+              {/* <!-- Name --> */}
               <input
                 type="text"
-                id="defaultRegisterFormFirstName"
-                class="form-control"
-                placeholder="First name"
+                className="form-control"
+                placeholder="Author Name"
               />
-            </div>
-            <div class="col">
-              {/* <!-- Last name --> */}
-              <input
-                type="text"
-                id="defaultRegisterFormLastName"
-                class="form-control"
-                placeholder="Last name"
-              />
-            </div>
-          </div>
+            {/* </div> */}
+            {/* <div className="col"> */}
+              <div className="previewComponent">
+                <form onSubmit={e => this._handleSubmit(e)}>
+                  <input
+                    className="fileInput"
+                    type="file"
+                    onChange={e => this._handleImageChange(e)}
+                  />
+                  <button
+                    className="submitButton"
+                    type="submit"
+                    onClick={e => this._handleSubmit(e)}
+                  >
+                    Upload Image
+                  </button>
+                </form>
+                <div className="imgPreview">{$imagePreview}</div>
+              </div>
+              {/* <!-- img --> */}
+              {/* <input
+                type="img"
+                className="form-control"
+                placeholder="img"
+              /> */}
 
-          {/* <!-- E-mail --> */}
-          <input
-            type="email"
-            id="defaultRegisterFormEmail"
-            class="form-control mb-4"
-            placeholder="E-mail"
-          />
+              {/* <div id="mainApp" />
+              <div class="centerText">
+                <span>Checkout associated </span>
+                <a
+                  href="http://www.hartzis.me/react-image-upload/"
+                  target="_blank"
+                >
+                  blog post
+                </a> */}
+              {/* </div> */}
+            {/* </div> */}
+          {/* </div> */}
 
-          {/* <!-- Password --> */}
-          <input
-            type="password"
-            id="defaultRegisterFormPassword"
-            class="form-control"
-            placeholder="Password"
-            aria-describedby="defaultRegisterFormPasswordHelpBlock"
-          />
-          <small
-            id="defaultRegisterFormPasswordHelpBlock"
-            class="form-text text-muted mb-4"
-          >
-            At least 8 characters and 1 digit
-          </small>
-
-          {/* <!-- Phone number --> */}
+          {/* <!-- Title --> */}
           <input
             type="text"
-            id="defaultRegisterPhonePassword"
-            class="form-control"
-            placeholder="Phone number"
+            className="form-control mb-4"
+            placeholder="Post Title"
+          />
+
+          {/* <!-- Content --> */}
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Post Content"
+          />
+
+          {/* <!-- Tag --> */}
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Tag Name"
             aria-describedby="defaultRegisterFormPhoneHelpBlock"
           />
-          <small
-            id="defaultRegisterFormPhoneHelpBlock"
-            class="form-text text-muted mb-4"
-          >
-            Optional - for two step authentication
-          </small>
 
-          {/* <!-- Newsletter --> */}
-          <div class="custom-control custom-checkbox">
-            <input
-              type="checkbox"
-              class="custom-control-input"
-              id="defaultRegisterFormNewsletter"
-            />
-            <label
-              class="custom-control-label"
-              for="defaultRegisterFormNewsletter"
-            >
-              Subscribe to our newsletter
-            </label>
-          </div>
-
-          {/* <!-- Sign up button --> */}
-          <button class="btn btn-info my-4 btn-block" type="submit">
-            Sign in
+          {/* <!-- Submit button --> */}
+          <button className="btn my-4 btn-block" type="submit">
+            Submit
           </button>
         </form>
       </div>
@@ -100,4 +132,5 @@ NewPost.defaultProps = {
   sectionDescription:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac augue at erat hendrerit dictum. Praesent porta, purus eget sagittis imperdiet."
 };
+
 export default NewPost;
