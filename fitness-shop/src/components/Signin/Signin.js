05ba9e2a1e6db498a4f2cb9e7c54a2070c6d5cef
kate li <kateli24@outlook.com>
2019-06-06 16:50:55 +1200

    Created a form for signin

--- fitness-shop/src/components/Signin/Signin.js
+++ fitness-shop/src/components/Signin/Signin.js
@@ -1,10 +1,125 @@
-import React, {Component} from 'react';
-class Signin extends Component{
-    render(){
-        return (
-            <p>This is sign in page</p>
-
-        )
+import React, { Component } from "react";
+import css from "./Signin.module.css";
+import Input from "../Input/Input";
+class Signin extends Component {
+  state = {
+    controls: {
+      username: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Username"
+        },
+        value: "",
+        validation: {
+          required: true,
+          minLength: 3
+        },
+        valid: false,
+        touched: false
+      },
+      password: {
+        elementType: "input",
+        elementConfig: {
+          type: "password",
+          placeholder: "Password"
+        },
+        value: "",
+        validation: {
+          required: true,
+          minLength: 6
+        },
+        valid: false,
+        touched: false
+      }
     }
+  };
+
+  checkValidity(value, rules) {
+    let isValid = true;
+    if (!rules) {
+      return true;
+    }
+
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid;
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    return isValid;
+  }
+
+  inputChangedHandler = (event, controlName) => {
+    const updatedControls = {
+      ...this.state.controls,
+      [controlName]: {
+        ...this.state.controls[controlName],
+        value: event.target.value,
+        valid: this.checkValidity(
+          event.target.value,
+          this.state.controls[controlName].validation
+        ),
+        touched: true
+      }
+    };
+    this.setState({ controls: updatedControls });
+  };
+
+  submitHandler = event => {
+    event.preventDefault();
+    // this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value,this.state.ifSignup);
+  };
+
+  render() {
+    const formElementsArray = [];
+    for (let key in this.state.controls) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.controls[key]
+      });
+    }
+
+    const form = formElementsArray.map(formElement => (
+      <Input
+        key={formElement.id}
+        elementType={formElement.config.elementType}
+        elementConfig={formElement.config.elementConfig}
+        value={formElement.config.value}
+        invalid={!formElement.config.valid}
+        shouldValidate={formElement.config.validation}
+        touched={formElement.config.touched}
+        changed={event => this.inputChangedHandler(event, formElement.id)}
+      />
+    ));
+
+    return (
+      <div className={css.Signin}>
+        <h1>Sign in</h1>
+        <form onSubmit={this.submitHandler}>
+          {form}
+          <p>New member? Please <span><strong>sign up</strong></span></p>
+          <button>Submit </button>
+        </form>
+      </div>
+    );
+  }
 }
+
 export default Signin;
