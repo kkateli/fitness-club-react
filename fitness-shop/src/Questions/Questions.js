f073630408594c137eb9177e35328c74fd35882f
kate li <kateli24@outlook.com>
2019-06-12 16:59:39 +1200

    Created a search bar

--- fitness-shop/src/Questions/Questions.js
+++ fitness-shop/src/Questions/Questions.js
@@ -1,12 +1,32 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Question from "../Question/Question";
-class Questions extends Component{
-render(){
-    return(
+import cssClasses from "./Questions.module.css";
+
+
+class Questions extends Component {
+  render() {
+    return (
+      <div>
+        <div className="section-title">
+          <h2>Questions</h2>
+
+          <form className="w-100 d-none d-md-flex d-lg-flex">
+            <container>
+            <i class="icofont-search"></i>
+            <input type="text" className={cssClasses.searchBar}  placeholder="Search for something..."  /> 
+            </container>
+         
+               
+                 
+               
+             
+           
+          </form>
+          
+        </div>
         <Question />
-
-
-    )
-}
+      </div>
+    );
+  }
 }
 export default Questions;
