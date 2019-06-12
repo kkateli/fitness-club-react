c528db069a6ecd554eedab8aa8b7a2b97393a55f
kate li <kateli24@outlook.com>
2019-06-12 20:28:19 +1200

    Embeded question comp to questions comp

--- fitness-shop/src/Question/Question.js
+++ fitness-shop/src/Question/Question.js
@@ -1,13 +1,19 @@
 import React from "react";
-import cssClasses from "./Question.module.css";
-const question = (props)=>{
-    return(
-        <div className={cssClasses.question}>
-            <div className={cssClasses.title}>Question Title</div>
-            <div className={cssClasses.content}>Content</div>
+
+import "./Question.css";
+
+const question = props => {
+  return (
+    <div className='question'>
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title searchTitle" >{props.title}</h5>
+          <p className="card-text searchContent">
+            {props.content}
+          </p>
         </div>
-
-    )
-
-}
+      </div>
+    </div>
+  );
+};
 export default question;
--- fitness-shop/src/Questions/Questions.js
+++ fitness-shop/src/Questions/Questions.js
@@ -1,30 +1,57 @@
 import React, { Component } from "react";
 import Question from "../Question/Question";
 import cssClasses from "./Questions.module.css";
-
+import axios from "axios";
 
 class Questions extends Component {
+  state = {
+    questions: []
+  };
+  //Ajax calls
+  componentDidMount() {
+    axios
+      .get("https://search-engine-e54b4.firebaseio.com/.json")
+      .then(response => {
+        this.setState({
+          questions: response.data
+        });
+      })
+
+      .then(response => {
+        console.log(this.state.questions);
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   render() {
+    const questionList = this.state.questions.map((e,index) => {
+      
+      return (
+        
+        <div key={index}>
+          <Question title={e.title} content={e.content}/>
+        </div>
+      );
+    });
     return (
-      <div>
+      <div className={cssClasses.questions}>
         <div className="section-title">
           <h2>Questions</h2>
 
-          <form className="w-100 d-none d-md-flex d-lg-flex">
-            <container>
-            <i class="icofont-search"></i>
-            <input type="text" className={cssClasses.searchBar}  placeholder="Search for something..."  /> 
-            </container>
-         
-               
-                 
-               
-             
-           
+          <form className={cssClasses.searchEngine}>
+            <i className="icofont-search" />
+            <input
+              type="text"
+              className={cssClasses.searchBar}
+              placeholder="Search for something..."
+            />
           </form>
-          
+
+          {questionList}
         </div>
-        <Question />
       </div>
     );
   }
