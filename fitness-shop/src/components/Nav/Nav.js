6a1a1a9b7091fd6102b183f8d61d77b9a96104aa
kate li <kateli24@outlook.com>
2019-06-16 21:02:42 +1200

    Added nav button

--- fitness-shop/src/components/Nav/Nav.js
+++ fitness-shop/src/components/Nav/Nav.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Icofont from "react-icofont";
-
+import "./Nav.css";
 import { Link } from "react-router-dom";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import Sidebar from "../SideBar/SideBar";
@@ -15,7 +15,6 @@ class NavBar extends Component {
     this.setState({ menuClicked: !this.state.menuClicked });
   };
 
-  
   render() {
     let sidebar = null;
     if (this.state.menuClicked) {
@@ -29,187 +28,136 @@ class NavBar extends Component {
 
     return (
       <div>
-        {/* Start Top Header */}
-        <div className="top-header">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-7 col-lg-7">
-                <div className="address-bar">
-                  <ul className="list-inline">
-                    <li>
-                      <a href={this.props.mailLink}>
-                        <Icofont icon="icofont-email" /> {this.props.mail}
-                      </a>
-                    </li>
-                    <li>
-                      <a href={this.props.numberLink}>
-                        <Icofont icon="icofont-ui-call" /> {this.props.Number}
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-
-              <div className="col-lg-5 col-md-5">
-                <div className="social-icons">
-                  <ul className="list-inline">
-                    <li>
-                      <a
-                        href={this.props.facebookLink}
-                        rel="noopener noreferrer"
-                        target="_blank"
-                      >
-                        <Icofont icon="icofont-facebook" />
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href={this.props.twitterLink}
-                        rel="noopener noreferrer"
-                        target="_blank"
-                      >
-                        <Icofont icon="icofont-twitter" />
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href={this.props.instagramLink}
-                        rel="noopener noreferrer"
-                        target="_blank"
-                      >
-                        <Icofont icon="icofont-instagram" />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="nav-menu" onClick={this.clickMenuHandler}>
+          <i className="icofont-navigation-menu" />
         </div>
-        {/* End Top Header */}
-
-        <Navbar
-          id="navbar"
-          bg="light"
-          expand="lg"
-          className="navbar navbar-expand-md navbar-light"
-          collapseOnSelect={true}
-        >
-          <div className="nav-menu" onClick={this.clickMenuHandler}>
-            <i className="icofont-navigation-menu" />
-          </div>
-          <Container>
-            <Navbar.Brand className="navbar-brand logo">
-              <div>
-                <Link to="/">
-                  <img src={this.props.MainLogo} alt="Logo" />
-                </Link>
-              </div>
-            </Navbar.Brand>
-
-            <Navbar.Collapse id="navbarSupportedContent">
-              <Nav className="navbar-nav ml-auto">
+        {sidebar}
+        <div className="real-nav">
+          <Navbar
+            id="navbar"
+            bg="light"
+            expand="lg"
+            className="navbar navbar-expand-md navbar-light"
+            collapseOnSelect={true}
+          >
+            <Container>
+              <Navbar.Brand className="navbar-brand logo">
                 <div>
-                  <Nav.Item>
-                    <Link
-                      activeclass="active"
-                      to="/"
-                      className="smooths nav-link"
-                      onClick={this.closeNavbar}
-                    >
-                      Home
-                    </Link>
-                  </Nav.Item>
+                  <Link to="/">
+                    <img src={this.props.MainLogo} alt="Logo" />
+                  </Link>
+                </div>
+              </Navbar.Brand>
 
-                  <Nav.Item>
-                    <Link
-                      activeclass="active"
-                      to="whatWeDo"
-                      className="smooths nav-link"
-                      onClick={this.closeNavbar}
-                    >
-                      What we do
-                    </Link>
-                  </Nav.Item>
-                  {!this.props.ifAuth ? (
+              <Navbar.Collapse id="navbarSupportedContent">
+                <Nav className="navbar-nav ml-auto">
+                  <div>
                     <Nav.Item>
                       <Link
                         activeclass="active"
-                        to="signin"
+                        to="/"
                         className="smooths nav-link"
                         onClick={this.closeNavbar}
                       >
-                        Sign in
+                        Home
                       </Link>
                     </Nav.Item>
-                  ) : null}
 
-                  {this.props.ifAuth ? (
                     <Nav.Item>
                       <Link
                         activeclass="active"
-                        to="activities"
+                        to="whatWeDo"
                         className="smooths nav-link"
                         onClick={this.closeNavbar}
                       >
-                        Activities
+                        What we do
                       </Link>
                     </Nav.Item>
-                  ) : null}
-                  {this.props.ifAuth ? (
+                    {!this.props.ifAuth ? (
+                      <Nav.Item>
+                        <Link
+                          activeclass="active"
+                          to="signin"
+                          className="smooths nav-link"
+                          onClick={this.closeNavbar}
+                        >
+                          Sign in
+                        </Link>
+                      </Nav.Item>
+                    ) : null}
+
+                    {this.props.ifAuth ? (
+                      <Nav.Item>
+                        <Link
+                          activeclass="active"
+                          to="activities"
+                          className="smooths nav-link"
+                          onClick={this.closeNavbar}
+                        >
+                          Activities
+                        </Link>
+                      </Nav.Item>
+                    ) : null}
+                    {this.props.ifAuth ? (
+                      <Nav.Item>
+                        <Link
+                          activeclass="active"
+                          to="memberPost"
+                          className="smooths nav-link"
+                          onClick={this.closeNavbar}
+                        >
+                          Member Posts
+                        </Link>
+                      </Nav.Item>
+                    ) : null}
+                    {this.props.ifAuth ? (
+                      <Nav.Item>
+                        <Link
+                          activeclass="active"
+                          to="newPost"
+                          className="nav-link"
+                          onClick={this.closeNavbar}
+                        >
+                          New Post
+                        </Link>
+                      </Nav.Item>
+                    ) : null}
+
                     <Nav.Item>
                       <Link
                         activeclass="active"
-                        to="memberPost"
-                        className="smooths nav-link"
-                        onClick={this.closeNavbar}
-                      >
-                        Member Posts
-                      </Link>
-                    </Nav.Item>
-                  ) : null}
-                  {this.props.ifAuth ? (
-                    <Nav.Item>
-                      <Link
-                        activeclass="active"
-                        to="newPost"
+                        to="documentation"
                         className="nav-link"
                         onClick={this.closeNavbar}
                       >
-                        New Post
+                        Documentation
                       </Link>
                     </Nav.Item>
-                  ) : null}
+                  </div>
+                </Nav>
+              </Navbar.Collapse>
 
-                  <Nav.Item>
-                    <Link
-                      activeclass="active"
-                      to="documentation"
-                      className="nav-link"
-                      onClick={this.closeNavbar}
-                    >
-                      Documentation
-                    </Link>
-                  </Nav.Item>
-                </div>
-              </Nav>
-            </Navbar.Collapse>
-
-            {this.props.ifAuth ? (
-              <p className="logoutButton">
-                Hi, {this.props.userEmail.split("@")[0]}
-                <Link to={"/logout"}> Log out</Link>
-              </p>
-            ) : null}
-          </Container>
-        </Navbar>
-
-        <div className="sub-logo">
-          <Link to="/">
-            <img src={this.props.MainLogo} alt="Logo" />
-          </Link>
+              {this.props.ifAuth ? (
+                <p className="logoutButton">
+                  Hi, {this.props.userEmail.split("@")[0]}
+                  <Link to={"/logout"}> Log out</Link>
+                </p>
+              ) : null}
+             
+            </Container>
+            
+          </Navbar>
+          <div className="mark-wrap">
+          <div className="dropup-mark"><i class="icofont-caret-up"></i></div>
+          </div>
+         
+          {/* <div className="sub-logo">
+            <Link to="/">
+              <img src={this.props.MainLogo} alt="Logo" />
+            </Link>
+          </div> */}
+          
         </div>
-        {sidebar}
       </div>
     );
   }
